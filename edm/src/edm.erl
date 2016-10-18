774074e9b85c540f58f0cd92bffe66f65b08aab2
Mattias Landkvist <mattias.landkvist@gmail.com>
2016-10-18 18:24:01 +0200

    minor update

--- edm/src/edm.erl
+++ edm/src/edm.erl
@@ -1,62 +1,66 @@
 -module(edm).
 
 %% API exports
--export([]).
+-export([start/0]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
-
+start() -> spawn(fun () -> init() end).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
-start() -> 
+init() -> 
 	{ok, Client} = emqttc:start_link([{host, "176.10.136.208"}, {client_id, <<"simpleClient">>}, {proto_ver, 3}]),
-	emqttc:subscribe(Client, [{<<”web/deal/new”>>, 1},
-					{<<”web/deal/edit”>>, 1},
-					{<<”web/deal/delete”>>, 1},
-					{<<”web/user/register”>>, 1},
-					{<<”web/user/edit”>>, 1},
-					{<<”web/user/delete”>>, 1},
-					{<<”app/deal/save”>>, 1},
-					{<<”app/deal/delete”>>, 1},
-					{<<”app/deal/verify”>>, 1},
-					{<<”app/user/new”>>, 1},
-					{<<”app/user/filter”>>, 1}]),
+	emqttc:subscribe(Client, [{<<"web/deal/new">>, 1},
+					{<<"web/deal/edit">>, 1},
+					{<<"web/deal/delete">>, 1},
+					{<<"web/user/register">>, 1},
+					{<<"web/user/edit">>, 1},
+					{<<"web/user/delete">>, 1},
+					{<<"app/deal/save">>, 1},
+					{<<"app/deal/delete">>, 1},
+					{<<"app/deal/verify">>, 1},
+					{<<"app/user/new">>, 1},
+					{<<"app/user/filter">>, 1}]),
 
 	{ok, Database} = mysql:start_link([{host, "localhost"}, {user, "user"}, {password, "password"}, {database, "test"}]),
-        Broker = spawn(fun () -> broker_loop(Client) end),
+        
+	Broker = spawn(fun () -> broker_loop(Client) end),
 	Database = spawn(fun () -> database_loop(Database) end),
-        register(broker,Broker),
+        
+	register(broker,Broker),
 	register(database, Database).
 
 	
 
 broker_loop(Client) ->
 	receive
- 	%% Receive messages from subscribed topics
+ 	
+	%% Receive messages from subscribed topics
         {{publish, Topic, Payload}} ->
                 database ! {Topic, Payload},
 		io:format("Message from ~s: ~p~n", [Topic, Payload]),
-                broker_loop(C);
+                broker_loop(Client);
 
         %% Publish messages with a topic and Qos to the broker
         {publish, Topic, Message, Qos} ->
                 Payload = list_to_binary([Message]),
-                emqttc:publish(C, Topic, Payload, [{qos, Qos}]),
-                broker_loop(C);
+                emqttc:publish(Client, Topic, Payload, [{qos, Qos}]),
+                broker_loop(Client);
 
         %% Subscribe to a topic on the broker
         {subscribe, Topic} ->
-                emqttc:subscribe(C, Topic, qos0),
-                broker_loop(C);
+                emqttc:subscribe(Client, Topic, qos0),
+                broker_loop(Client);
 
         %% Stop the loop as a part of stopping the client
         stop ->
                 ok
+	
 	end.
 
 database_loop(Database) ->
@@ -64,7 +68,7 @@ database_loop(Database) ->
 		{"web/deal/new", Payload} -> 
 			mysql:query(Database, "INSERT INTO deals (
 				client_id, name, price, picture, description, 
-				duration, count) VALUES (?,?,?,?,?,?)", [Message],
+				duration, count) VALUES (?,?,?,?,?,?)", [Message]),
 			broker ! {publish, <<"database/deal/location">>, NewMessage},
 			database_loop(Database);
 
@@ -77,7 +81,7 @@ database_loop(Database) ->
 			database_loop(Database);
 
 		{"web/deal/delete", Payload} -> 
-			mysql:query(Database, "DELETE FROM deals WHERE client_id = ?" 
+			mysql:query(Database, "DELETE FROM deals WHERE client_id = ?", 
 				[Message]),
 			broker ! {publish, <<"database/deal/location">>, NewMessage},
 			database_loop(Database);
@@ -92,7 +96,7 @@ database_loop(Database) ->
 			database_loop(Database);
 
 		{"web/user/delete", Payload} -> 
-			mysql:query(Database, "DELETE FROM clients WHERE id = ?" [Message]),
+			mysql:query(Database, "DELETE FROM clients WHERE id = ?", [Message]),
 			database_loop(Database);
 
 		{"app/deal/save", Payload} -> 
@@ -127,5 +131,5 @@ database_loop(Database) ->
 			ClientInfo = mysql:query(Database, "SELECT * FROM clients WHERE id = ?", [Id]),
 			broker ! {publish, <<"database/info/client">>, ClientInfo};
 		
-		stop -> exit(database, normal);
+		stop -> exit(database, normal)
 	end.
